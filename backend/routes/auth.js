a2ca3a203e5a1cb29b9e5430a942960d379fa4bc
GnuBurn <ausakorn1909@gmail.com>
2025-11-03 01:47:31 +0700

    Add security

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,5 +8,6 @@ const {protect} = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login',login);
 router.get('/me', protect, getMe);
+router.get('/logout',logout);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,56 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
-const cors = require('cors'); 
+const mongoSanitize=require('@exortek/express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp = require('hpp');
+const cors=require('cors');
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUI=require('swagger-ui-express');
+
 
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
 app.set('query parser', 'extended');
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(hpp());
+app.use(cors());
+
+const limiter=rateLimit({
+    windowsMs:10*60*1000, //10mins
+    max: 100
+});
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+app.use(limiter);
 
 connectDB();
 
